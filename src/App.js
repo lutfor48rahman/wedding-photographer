65ca1f6a539d3a729b0bdf2c77892aa2aa5961fe
Lutfor Rahman <96254692+lutfor48rahman@users.noreply.github.com>
2022-04-18 00:41:03 +0600

    banner add

--- src/App.js
+++ src/App.js
@@ -8,17 +8,20 @@ import Footer from './components/Footer/Footer';
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/blog' element={<Blog></Blog>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<Signup></Signup>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,22 @@
+import { Button } from 'bootstrap';
+import { Carousel } from 'bootstrap';
 import React from 'react';
+import banner1 from '../images/banner1.jpg';
+import banner3 from '../images/banner3.jpg';
 
 const Home = () => {
     return (
         <div>
-            <h2>This is home page</h2>
+            <div id="carouselExampleControls" className="carousel slide mt-3" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img className='d-block w-100 h-25' src={banner1} alt="" />
+                    </div>
+                    
+                </div>
+            </div>
+            <h1>This is home page</h1>
+
         </div>
     );
 };
