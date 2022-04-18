d165f07bfc5fc25a29922adda05de6a4b463cf17
Lutfor Rahman <96254692+lutfor48rahman@users.noreply.github.com>
2022-04-18 23:09:36 +0600

    about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,6 +4,7 @@ const About = () => {
     return (
         <div>
             <h2>About myself</h2>
+            <p className='que1'>My name is <b> Md.Rashed Khan Milon</b> . I  am a hard learner with web development . I want to be a good web-developer. I am already working hard and I will do my best to develop myself. I want to go abroad for higher studies.And then I want to go to the job market with my development skill.I think it will help me a lot to get a good career.</p>
         </div>
     );
 };
